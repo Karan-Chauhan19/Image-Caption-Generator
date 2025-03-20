d4c4e4c37c74ece547d5d1bc67c8abf4784eaaf4
karan190806 <kc879022@gmail.com>
2025-03-20 23:11:55 +0530

    added train.py file

--- src/train.py
+++ src/train.py
@@ -5,79 +5,60 @@ organization: L.J Unniversity
 """
 
 import torch
+import torch.nn as nn
 import torch.optim as optim
 from nn_arch.vgg16 import VGG16
 from config.config import Config
-from utils.tokenizer import Tokenizer
+from dataset import CaptionDataset
 from gpu_config.check import check_gpu
-from dataset import ImageCaptionDataset
 from torch.utils.data import DataLoader
+from utils.embeddings import EmbeddingHandler
 from nn_arch.caption_model import CaptionModel
-from utils.caption_processor import CaptionProcessor
 
 def main():
     check_gpu()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
-    # Feature extraction
-    extractor = VGG16(device)
-    image_dir = Config().TRAIN_IMAGE_PATH
-    features = extractor.extract_features(image_dir)
-    features = extractor.load_features()
-    
-    # Caption processing
-    processor = CaptionProcessor()
-    mapping = processor.load_captions()
-    processor.clean_captions()
-    all_captions = processor.get_all_captions()
-    
-    # Tokenization
-    tokenizer = Tokenizer()
-    tokenizer.fit_on_texts(all_captions)
-    vocab_size = tokenizer.get_vocab_size()
-    max_length = max(len(caption.split()) for caption in all_captions)
-    
-    # Data split
+    embedding = EmbeddingHandler()
+    vgg = VGG16()
+
+    mapping = EmbeddingHandler().process_captions(Config().TRAIN_CAPTIONS_PATH)
+    features = vgg.extract_features(Config().TRAIN_IMAGE_PATH)
+    caption_embeddings = embedding.generate_embeddings(mapping)
+    max_length = embedding.get_max_length(mapping)
     image_ids = [img_id for img_id in mapping.keys() if img_id in features]
     split = int(len(image_ids) * 0.95)
     train = image_ids[:split]
-    test = image_ids[split:]
-    
-    # Dataset and DataLoader
-    train_dataset = ImageCaptionDataset(train, mapping, features, tokenizer, max_length)
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    
-    # Model training
-    model = CaptionModel(vocab_size, max_length).to(device)
-    criterion = torch.nn.CrossEntropyLoss(ignore_index=0)
+
+    #Training
+    batch_size = 16
+    train_dataset = CaptionDataset(train, mapping, features, caption_embeddings, max_length)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+    model = CaptionModel(embedding_dim=384).to(device)
+    criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
-    
+
     epochs = 50
+    
     for epoch in range(epochs):
-        model.train()
         total_loss = 0
-        for batch_idx, (img_features, captions, targets) in enumerate(train_loader):
+        model.train()
+        for batch_idx, (img_features, caption_embeddings) in enumerate(train_loader):
             img_features = img_features.to(device)
-            captions = captions.to(device)
-            targets = targets.to(device)
-            
+            caption_embeddings = caption_embeddings.to(device)
+
             optimizer.zero_grad()
-            outputs = model(img_features, captions)
-            
-            outputs = outputs.view(-1, vocab_size)
-            targets = targets.view(-1)
-            
-            loss = criterion(outputs, targets)
+            outputs = model(img_features, caption_embeddings)
+
+            loss = criterion(outputs, caption_embeddings)
             loss.backward()
             optimizer.step()
-            
+
             total_loss += loss.item()
-        
+
         avg_loss = total_loss / len(train_loader)
         print(f'Epoch [{epoch+1}/{epochs}], Loss: {avg_loss:.4f}')
-    
-    torch.save(model.state_dict(), Config().PATH_TO_SAVE_TRAINED_MODEL +'best_model.pt')
 
-if __name__ == "__main__":
-    main()
+    torch.save(model.state_dict(), Config().PATH_TO_SAVE_TRAINED_MODEL)
